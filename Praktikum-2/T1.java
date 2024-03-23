198e0be13d112bb12fc35ff408e854f8334ac0b4
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-23 11:09:52 +0800

    P2 T1

---
+++ Praktikum-2/T1.java
@@ -0,0 +1,29 @@
+class Person {
+    String name;
+    int age;
+    boolean isMale;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+}
